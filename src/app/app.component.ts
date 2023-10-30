1d9e2d7e30f9c6602f37c45212eac85427b9ea6b
PanTer <panosterzakis11@gmail.com>
2023-10-30 20:36:43 +0200

    Event-Bind-css-fx-layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,5 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PersonComponent } from './person/person.component';
-import { Person } from './interfaces/person';
-import { PersonAltComponent } from './person-alt/person-alt.component';
-import { EventBindComponent } from './event-bind/event-bind.component';
-import { OutputDemoComponent } from './output-demo/output-demo.component';
-import { PersonCardComponent } from './person-card/person-card.component';
-import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
-import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
-import { AppService } from './app.service';
-import { CrudDemoComponent } from './crud-demo/crud-demo/crud-demo.component';
 import { ApplicationLayoutComponent } from './application-layout/application-layout.component';
 
 @Component({
@@ -17,63 +7,9 @@ import { ApplicationLayoutComponent } from './application-layout/application-lay
   standalone: true,
   imports: [
     CommonModule,
-    PersonComponent, 
-    PersonAltComponent, 
-    EventBindComponent, 
-    OutputDemoComponent, 
-    PersonCardComponent, 
-    TemplateDrivenFormComponent,
-    ReactiveFormComponent,
-    CrudDemoComponent,
     ApplicationLayoutComponent,
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  name: string = "Panagiotis";
-  lastname = "Terzakis";
-
-    person: Person = {
-    givenName: 'Pan',
-    surName:'Terz',
-    age: 30,
-    email: 'pan@gmail.com',
-    address: 'Athens'
-  };
-
-  person2: Person = {
-    givenName: 'Nikos',
-    surName:'Pappas',
-    age: 40,
-    email: 'nik@gmail.com',
-    address: 'London'
-  };
-    
-users: Person[] = [];
-
-sendUser: Person | undefined;
-
-constructor(private appService: AppService = Inject(AppService)) {}
-
-ngOnInit(): void {
-  this.appService.getAllUsers().subscribe((users) => {
-    this.users = users
-    console.log(this.users)
-  })
-}
-
-onDeleteUser(i:number){
-  this.users.splice(i,1)
-}
-
-onSendUser(user: Person){
-  console.log(user)
-  this.sendUser = user;
-}
-
-onNewPerson(person: Person){
-  this.users.push(person);
-}
-  
-}
+export class AppComponent {}
